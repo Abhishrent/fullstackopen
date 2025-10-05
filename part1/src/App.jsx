4397437b17a5990d8567c466e834e9ff7e91a62a
abhishrent <abhishrent+github@gmail.com>
2025-10-05 14:53:20 +0545

    1.5: course information step5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,48 +1,42 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  
-
-
-	return ( 
-		<div> 
-			<Header course={course} />
-			<Content 
-				parts={parts}
-			/>
-			<Total parts={parts} />
-		</div>
-	)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+	<>
+	<Header course={course} />
+	<Content course={course} />
+	<Total course={course} />
+	</>
+  )
 }
-
 
 const Header = (props) => {
 	return (
 		<div>
-		<h1>{props.course}</h1>
+		<h1>{props.course.name}</h1>
 		</div>
 	)
 }
 
-
-
 const Part = (props) => {
 	return (
 		<div>
-			{props.name} {props.exercise}
+			{props.part.name} {props.part.exercises}
 		</div>
 	)
 }
@@ -50,15 +44,15 @@ const Part = (props) => {
 const Content = (props) => {
 	return (
 		<div> 
-			<Part name={props.parts[0].name} exercise={props.parts[0].exercises} />
-			<Part name={props.parts[1].name} exercise={props.parts[1].exercises} />
-			<Part name={props.parts[2].name} exercise={props.parts[2].exercises} />
+			<Part part={props.course.parts[0]} />
+			<Part part={props.course.parts[1]} />
+			<Part part={props.course.parts[2]} />
 		</div>
 	)
 }
 
 const Total = (props) => { 
-	const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+	const total = props.course.parts.reduce((sum, part) => sum + part.exercises, 0)
 	return (
 		<p> Number of exercises {total}</p>
 	)
