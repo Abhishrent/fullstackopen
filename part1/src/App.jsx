a050019964d146a01e45a6ae932b70af650d4822
abhishrent <abhishrent+github@gmail.com>
2026-02-12 14:43:30 +0545

    1.6: unicafe step 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,61 +1,36 @@
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+import { useState } from 'react'
+
+const Button = ({onClick, text}) => {
+  return <button onClick={onClick}> {text} </button>
+}
+
+const Stats = ({good, neutral, bad}) => {
   return (
-	<>
-	<Header course={course} />
-	<Content course={course} />
-	<Total course={course} />
-	</>
+  <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+  </div>
   )
 }
 
-const Header = (props) => {
-	return (
-		<div>
-		<h1>{props.course.name}</h1>
-		</div>
-	)
-}
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-const Part = (props) => {
-	return (
-		<div>
-			{props.part.name} {props.part.exercises}
-		</div>
-	)
-}
+  return (
+  <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text='good' />
+      <Button onClick={() => setNeutral(neutral+1)} text='neutral' />
+      <Button onClick={() => setBad(bad+1)} text='bad' />
 
-const Content = (props) => {
-	return (
-		<div> 
-			<Part part={props.course.parts[0]} />
-			<Part part={props.course.parts[1]} />
-			<Part part={props.course.parts[2]} />
-		</div>
-	)
-}
-
-const Total = (props) => { 
-	const total = props.course.parts.reduce((sum, part) => sum + part.exercises, 0)
-	return (
-		<p> Number of exercises {total}</p>
-	)
+      <h1>statistics</h1>
+      <Stats good={good} neutral={neutral} bad={bad} />
+  </div>
+  )
 }
 
 export default App
+
