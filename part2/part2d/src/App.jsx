89d06f449412933620fecfe730118a22f907e072
Abhishrent Khatri <abhishrent+github@gmail.com>
2026-04-07 12:08:17 +0545

    2.15*: The Phonebook step 10

--- part2/part2d/src/App.jsx
+++ part2/part2d/src/App.jsx
@@ -4,48 +4,42 @@ import Form from './components/Form'
 import requestService from './services/requestService'
 
 const App = () => {
-
   const [filterTerm, setFilterTerm] = useState('')
   const [newPerson, setNewPerson] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [persons, setPersons] = useState([])
-  
-  //fetching data from the json-server
+
   useEffect(() => {
-    console.log('useEffect called')
     requestService.getAll()
-    .then(initialPeople => setPersons(initialPeople))
- }, [])
+      .then(initialPeople => setPersons(initialPeople))
+  }, [])
 
-  const handleInputSearchBar = (event) => {
-    console.log('filter input field updated to', event.target.value)
-    setFilterTerm(event.target.value) 
-  }
-  const handleInputPerson = (event) => {
-    console.log('name input field updated to', event.target.value)
-    setNewPerson(event.target.value)
-  }
-
-  const handleInputNumber = (event) => {
-    console.log('number input field updated to', event.target.value)
-    setNewNumber(event.target.value)
-  }
+  const handleInputSearchBar = (event) => setFilterTerm(event.target.value)
+  const handleInputPerson = (event) => setNewPerson(event.target.value)
+  const handleInputNumber = (event) => setNewNumber(event.target.value)
 
   const addNewPerson = (event) => {
     event.preventDefault()
-    const personObject = {
-      name: newPerson,
-      number: newNumber 
-    }
-
+    const personObject = { name: newPerson, number: newNumber }
     const hasName = persons.some(person => person.name === newPerson)
-    if(hasName) {
-      alert(`${newPerson} already in list!!!`)
-    }else{
+
+    if (hasName) {
+      if (window.confirm(`${newPerson} is already added to phonebook, replace the old number with a new one?`)) {
+        const personToUpdate = persons.find(p => p.name === newPerson)
+        const updatedPerson = { ...personToUpdate, number: newNumber }
+        requestService.updateNumber(personToUpdate.id, updatedPerson)
+          .then(returnedPerson => {
+            console.log('updated person:', returnedPerson)
+            setPersons(persons.map(p => p.id !== personToUpdate.id ? p : returnedPerson))
+          })
+          .catch(() => {
+            alert(`The person '${newPerson}' was already deleted from the server.`)
+            setPersons(persons.filter(p => p.id !== personToUpdate.id))
+          })
+      }
+    } else {
       requestService.create(personObject)
-      .then(newPersonInfo => {
-        setPersons(persons.concat(newPersonInfo))
-      })
+        .then(newPersonInfo => setPersons(persons.concat(newPersonInfo)))
     }
   }
 
@@ -53,35 +47,30 @@ const App = () => {
     const person = persons.find(p => p.id === id)
     if (window.confirm(`Delete ${person.name}?`)) {
       requestService.remove(id)
-        .then((response) => {
-          console.log('server delete response:', response)
-          setPersons(persons.filter(p => p.id !== id))
-        })
-        .catch(error => {
+        .then(() => setPersons(persons.filter(p => p.id !== id)))
+        .catch(() => {
           alert(`The person '${person.name}' was already deleted from the server.`)
           setPersons(persons.filter(p => p.id !== id))
         })
     }
   }
 
-
   return (
     <div>
-        <h1>Petu's Phonebook</h1>
-        <div>
-          Search <input value={filterTerm} onChange={handleInputSearchBar}/>
-        </div>
-        <h3>Add Number</h3>
-        <Form
-          addNewPerson={addNewPerson}
-          newPerson={newPerson}
-          newNumber={newNumber}
-          handleInputNumber={handleInputNumber}
-          handleInputPerson={handleInputPerson}
-        />
-        <h3>Numbers</h3>
-        <SearchBar persons={persons} filterTerm={filterTerm} handleDelete={handleDelete} />
+      <h1>Petu's Phonebook</h1>
+      <div>Search <input value={filterTerm} onChange={handleInputSearchBar}/></div>
+      <h3>Add Number</h3>
+      <Form
+        addNewPerson={addNewPerson}
+        newPerson={newPerson}
+        newNumber={newNumber}
+        handleInputNumber={handleInputNumber}
+        handleInputPerson={handleInputPerson}
+      />
+      <h3>Numbers</h3>
+      <SearchBar persons={persons} filterTerm={filterTerm} handleDelete={handleDelete} />
     </div>
   )
 }
+
 export default App
